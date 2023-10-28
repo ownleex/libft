8081e756d2b41c6b685d87334568f52b36992f4e
ownleex <ownleex@gmail.com>
2023-10-29 00:09:13 +0200

    change

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 18:42:05 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/10/28 23:50:21 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/10/29 00:07:46 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-size_t	ft_strlcpy(char *dest const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 //size_t	ft_strlcat(char *dest, const char *src, size_t size);
 //int		ft_toupper(int c);
 //int		ft_tolower(int c);
