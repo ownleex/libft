a8ec9cb6e6c55aca482818cf676d530baec1e383
ownleex <ownleex@gmail.com>
2023-11-02 00:50:20 +0100

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,8 +6,49 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 14:25:13 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/10/29 14:38:40 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/02 00:47:21 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	res_d;
+	size_t	res_s;
+
+	i = ft_strlen(dst);
+	j = 0;
+	res_d = ft_strlen(dst);
+	res_s = ft_strlen(src);
+	if (dstsize < 1)
+		return (res_s + dstsize);
+	while (src[j] && i < dstsize - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	if (dstsize < res_d)
+		return (res_s + dstsize);
+	else
+		return (res_d + res_s);
+}
+/*
+int	main(void)
+{
+	char dest[50] = "Hello, ";
+	char src[] = "world!";
+	unsigned int size = 50;
+	unsigned int result;
+
+	printf("Before ft_strlcat: %s\n", dest);
+	result = ft_strlcat(dest, src, size);
+	printf("After ft_strlcat: %s\n", dest);
+	printf("Returned value: %u\n", result);
+	return (0);
+}
+*/
