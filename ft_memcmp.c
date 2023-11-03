ca4c9d5fde6af39a465b8832eecc33af5b59e470
ownleex <ownleex@gmail.com>
2023-11-03 01:17:32 +0100

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,29 +6,33 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 23:38:07 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/02 23:45:31 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/03 01:17:18 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	size_t	i;
+	size_t				i;
+	const unsigned char	*s1;
+	const unsigned char	*s2;
 
+	s1 = (const unsigned char *)str1;
+	s2 = (const unsigned char *)str2;
 	i = 0;
-	while (i < n && *(unsigned char *)(s1 + i) == *(unsigned char *)(s2 + i))
+	while (i < n && s1[i] == s2[i])
 		i++;
 	if (i < n)
-		return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+		return (s1[i] - s2[i]);
 	return (0);
 }
 
 /*
 int	main(void)
 {
-	const char	s1[] = "bonnour";
-	const char	s2[] = "bonjour";
+	const char	s1[] = "bonjour";
+	const char	s2[] = "bonnour";
 	size_t	n = 5;
 
 	printf("%d\n", ft_memcmp(s1, s2, n));
