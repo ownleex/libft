38f93c3f7f72c2721d374d35d31b7af15ca023df
ownleex <ownleex@gmail.com>
2023-11-09 15:21:58 +0100

    Refactor ft_substr function

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:50:26 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/09 15:12:21 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/09 15:21:48 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 
 char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
-    char	*substr;
-    size_t	i;
-    size_t	str_len;
+	char	*substr;
+	size_t	i;
+	size_t	str_len;
 
-    if (str == NULL)
-	return (NULL);
-    str_len = ft_strlen(str);
-    if (start >= str_len)
-	return (ft_strdup(""));
-    len = (start + len > str_len) ? str_len - start : len;
-    substr = (char *)malloc(sizeof(char) * (len + 1));
-    if (substr == NULL)
-	return (NULL);
-    i = 0;
-    while (i < len && str[start] != '\0')
-    {
-	substr[i] = str[start];
-	i++;
-	start++;
-    }
-    substr[i] = '\0';
-    return (substr);
+	if (str == NULL)
+		return (NULL);
+	str_len = ft_strlen(str);
+		if (start >= str_len)
+			return (ft_strdup(""));
+	len = (start + len > str_len) ? str_len - start : len;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (substr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len && str[start] != '\0')
+	{
+		substr[i] = str[start];
+		i++;
+		start++;
+	}
+	substr[i] = '\0';
+	return (substr);
 }
 
 /*
