8419af09d9096eb673836887d679e781c7f9e352
ownleex <ownleex@gmail.com>
2023-11-12 18:26:20 +0100

    Refactor ft_split function to improve readability and fix memory leaks

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 18:17:41 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/12 18:25:36 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/12 18:26:15 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,38 +26,38 @@ char **ft_split(char const *s, char c)
     /* Compte le nombre de mots */
     while (s[i])
     {
-        if (s[i] != c)
-        {
-            n++;
-            while (s[i] != c && s[i])
-                i++;
-        }
-        else
-            i++;
+	if (s[i] != c)
+	{
+	    n++;
+	    while (s[i] != c && s[i])
+		i++;
+	}
+	else
+	    i++;
     }
 
     /* Alloue le tableau de mots */
     tab = malloc(sizeof(char *) * (n + 1));
     if (!tab)
-        return (NULL);
+	return (NULL);
 
     /* Parcourt la chaîne et copie les mots dans le tableau */
     while (n--)
     {
-        while (s[j] == c)
-            j++;
-        tab[n] = malloc(sizeof(char) * (j - i + 2));
-        if (!tab[n])
-        {
-            while (n < i)
-                free(tab[++n]);
-            free(tab);
-            return (NULL);
-        }
-        memcpy(tab[n], &s[j], j - i + 1);
-        tab[n][j - i + 1] = '\0';
-        j = i + 1;
-        i = j;
+	while (s[j] == c)
+	    j++;
+	tab[n] = malloc(sizeof(char) * (j - i + 2));
+	if (!tab[n])
+	{
+	    while (n < i)
+		free(tab[++n]);
+	    free(tab);
+	    return (NULL);
+	}
+	memcpy(tab[n], &s[j], j - i + 1);
+	tab[n][j - i + 1] = '\0';
+	j = i + 1;
+	i = j;
     }
     tab[n] = NULL;
 
