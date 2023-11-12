d4b151d01b703e884e8f384756bd62dd6fb63dab
ownleex <ownleex@gmail.com>
2023-11-12 02:10:13 +0100

    Refactor ft_split function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:39:17 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/12 01:55:43 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/12 02:08:31 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,6 @@ char	*ft_strdup(const char *str);
 char	*ft_substr(char const *str, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char    **ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c);
 
 #endif
