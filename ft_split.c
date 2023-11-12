0400e320507afdaa3b167ef54be1141d9d3df873
ownleex <ownleex@gmail.com>
2023-11-12 02:06:47 +0100

    Add ft_split function to library

---
+++ ft_split.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 01:56:48 by ayarmaya          #+#    #+#             */
+/*   Updated: 2023/11/12 02:06:36 by ayarmaya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_count_words(char const *s, char c)
+{
+        int	i;
+        int	count;
+
+        i = 0;
+        count = 0;
+        if (s[i] != c && s[i] != '\0')
+                count++;
+        while (s[i] != '\0')
+        {
+                if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+                        count++;
+                i++;
+        }
+        return (count);
+}
+
+static int	ft_word_len(char const *s, char c)
+{
+        int	i;
+        int	len;
+
+        i = 0;
+        len = 0;
+        while (s[i] != c && s[i] != '\0')
+        {
+                len++;
+                i++;
+        }
+        return (len);
+}
+
+static char	**ft_free(char **tab, int i)
+{
+        while (i >= 0)
+        {
+                free(tab[i]);
+                i--;
+        }
+        free(tab);
+        return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+        char	**tab;
+        int	i;
+        int	j;
+        int	k;
+
+        i = 0;
+        j = 0;
+        if (!s || !(tab = (char **)malloc(sizeof(char *) * (ft_count_words(s, c) + 1))))
+                return (NULL);
+        while (i < ft_count_words(s, c))
+        {
+                k = 0;
+                if (!(tab[i] = (char *)malloc(sizeof(char) * (ft_word_len(&s[j], c) + 1))))
+                        return (ft_free(tab, i));
+                while (s[j] == c)
+                        j++;
+                while (s[j] != c && s[j] != '\0')
+                        tab[i][k++] = s[j++];
+                tab[i][k] = '\0';
+                i++;
+        }
+        tab[i] = NULL;
+        return (tab);
+}
+
+/*
+int	main(void)
+{
+        char	**tab;
+        int	i;
+
+        i = 0;
+        tab = ft_split("salut les amis", ' ');
+        while (tab[i])
+        {
+                printf("%s\n", tab[i]);
+                i++;
+        }
+        return (0);
+}
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:39:17 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/11 12:27:46 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/12 01:55:43 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,6 @@ char	*ft_strdup(const char *str);
 char	*ft_substr(char const *str, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char    **ft_split(char const *s, char c);
 
 #endif
