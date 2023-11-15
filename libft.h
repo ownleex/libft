1e6e0b0452808bce5de52e4adaaf6e4224e899ca
ownleex <ownleex@gmail.com>
2023-11-15 20:50:06 +0100

    Fix typo in ft_putchar_fd function name

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:39:17 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/15 20:36:04 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/15 20:49:30 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,6 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-void	ft_putchar_fc(char c, int fd);
+void	ft_putchar_fd(char c, int fd);
 
 #endif
