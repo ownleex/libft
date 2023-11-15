e6969d33ac41c07a52cf8db00a0d6c0b6696ad25
ownleex <ownleex@gmail.com>
2023-11-15 18:50:29 +0100

    Fix function signature and formatting in ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,34 +6,34 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 18:39:43 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/15 18:49:03 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/15 18:50:24 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_striteri(char *, void (*f)(unsigned int, char*));
+void	ft_striteri(char *, void (*f)(unsigned int, char*));
 {
-        int     i;
+	int	i;
 
-        if (!s || !f)
-                return ;
-        i = 0;
-        while (s[i])
-        {
-                (f*)(i, &s[i]);
-                i++;
-        }
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		(f*)(i, &s[i]);
+		i++;
+	}
 }
 
 /*
 int     main(void)
 {
-        char    str[] = "Hello World!";
-        void    (*f)(unsigned int, char*);
+	char    str[] = "Hello World!";
+	void    (*f)(unsigned int, char*);
 
-        f = &ft_putchar;
-        ft_striteri(str, f);
-        return (0);
+	f = &ft_putchar;
+	ft_striteri(str, f);
+	return (0);
 }
 */
