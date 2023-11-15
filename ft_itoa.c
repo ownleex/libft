e237f636dda2a2f1f111df9f8fe170446e178cc4
ownleex <ownleex@gmail.com>
2023-11-15 17:27:31 +0100

    Refactor ft_itoa to improve performance and readability.

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,59 +6,57 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 17:15:55 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/15 17:24:32 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/15 17:27:22 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_nbrlen(long n)
-{
-	int		i;
 
-	i = 1;
+static int	num_digit(long num)
+{
+	int	cur;
+
+	cur = 0;
+	if (num == 0)
+		return (1);
+	if (num < 0)
+		cur++;
+	while (num != 0)
+	{
+		num = num / 10;
+		cur++;
+	}
+	return (cur);
+}
+
+char	*ft_itoa(int n)
+{
+	long	len;
+	long	nl;
+	char	*result;
+
+	len = num_digit(n);
+	nl = n;
 	if (n < 0)
-	{
-		n *= -1;
-		i++;
-	}
-	while (n > 9)
-	{
-		n /= 10;
-		i++;
-	}
-	return (i);
-}
-
-static void	ft_putnbr_stock(long n, char *str, int *i)
-{
-	if (n > 9)
-	{
-		ft_putnbr_stock(n / 10, str, i);
-		ft_putnbr_stock(n % 10, str, i);
-	}
-	else
-		str[(*i)++] = n + '0';
-}
-
-char		*ft_itoa(int n)
-{
-	char	*str;
-	int		i;
-	long	nbr;
-
-	nbr = n;
-	if ((str = malloc(sizeof(char) * (ft_nbrlen(nbr) + 1))) == NULL)
+		nl *= -1;
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
 		return (NULL);
-	i = 0;
-	if (nbr < 0)
+	result[len] = 0;
+	if (nl == 0)
+		result[0] = '0';
+	else
 	{
-		str[i++] = '-';
-		nbr *= -1;
+		while (len--, nl != 0)
+		{
+			result[len] = (nl % 10) + '0';
+			nl = (nl - (nl % 10)) / 10;
+		}
+		if (n < 0)
+			result[len] = '-';
 	}
-	ft_putnbr_stock(nbr, str, &i);
-	str[i] = '\0';
-	return (str);
+	return (result);
 }
 
 /*
