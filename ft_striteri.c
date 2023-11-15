19ea12e813f41d6d6edea0b6eab1e74a3d28ff29
ownleex <ownleex@gmail.com>
2023-11-15 19:44:43 +0100

    Refactor ft_striteri function

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,23 +6,23 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 18:39:43 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/15 18:53:02 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/15 19:44:37 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	unsigned int	i;
+	int	cur;
 
 	if (!s || !f)
 		return ;
-	i = 0;
-	while (s[i])
+	cur = 0;
+	while (s[cur])
 	{
-		(*f)(i, &s[i]);
-		i++;
+		(*f)(cur, &s[cur]);
+		cur++;
 	}
 }
 
