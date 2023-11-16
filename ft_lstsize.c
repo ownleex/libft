6b734ef27870cadd91d3c93c8833ba53dd8c068a
ownleex <ownleex@gmail.com>
2023-11-16 15:26:32 +0100

    Refactor ft_lstsize function

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,38 +6,38 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:19:39 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/16 15:25:36 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/16 15:26:27 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int     ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
-        size_t  i;
+	size_t  i;
 
-        i = 0;
-        while (lst)
-        {
-                lst = lst->next;
-                i++;
-        }
-        return (i);
+	i = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
 }
 
 /*
 int     main(void)
 {
-        t_list  *lst;
-        t_list  *new_lst;
-        t_list  *new_lst2;
+	t_list  *lst;
+	t_list  *new_lst;
+	t_list  *new_lst2;
 
-        lst = ft_lstnew("first");
-        new_lst = ft_lstnew("second");
-        new_lst2 = ft_lstnew("third");
-        ft_lstadd_front(&lst, new_lst);
-        ft_lstadd_front(&lst, new_lst2);
-        printf("%d\n", ft_lstsize(lst));
-        return (0);
+	lst = ft_lstnew("first");
+	new_lst = ft_lstnew("second");
+	new_lst2 = ft_lstnew("third");
+	ft_lstadd_front(&lst, new_lst);
+	ft_lstadd_front(&lst, new_lst2);
+	printf("%d\n", ft_lstsize(lst));
+	return (0);
 }
 */
