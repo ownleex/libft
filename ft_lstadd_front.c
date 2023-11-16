ecf2ae46887c067a327b0d49d41edeb2c3f128ed
ownleex <ownleex@gmail.com>
2023-11-16 01:51:35 +0100

    Refactor ft_lstadd_front function

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 01:45:11 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/16 01:48:40 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/16 01:51:30 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-        if (!lst || !new)
-                return ;
-        new->next = *lst;
-        *lst = new;
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
 }
 
 /*
 int     main(void)
 {
-        t_list  *lst;
-        t_list  *new_lst;
+	t_list  *lst;
+	t_list  *new_lst;
 
-        lst = ft_lstnew("first");
-        new_lst = ft_lstnew("second");
-        ft_lstadd_front(&lst, new_lst);
-        printf("%s\n", lst->content);
-        printf("%s\n", lst->next->content);
-        return (0);
+	lst = ft_lstnew("first");
+	new_lst = ft_lstnew("second");
+	ft_lstadd_front(&lst, new_lst);
+	printf("%s\n", lst->content);
+	printf("%s\n", lst->next->content);
+	return (0);
 }
 */
