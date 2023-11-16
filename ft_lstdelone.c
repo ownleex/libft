2fd6c822c25324a3f0be4df7db6eed39637d11d2
ownleex <ownleex@gmail.com>
2023-11-16 16:48:33 +0100

    Update Makefile and libft.h

---
+++ ft_lstdelone.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 16:45:52 by ayarmaya          #+#    #+#             */
+/*   Updated: 2023/11/16 16:48:23 by ayarmaya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	del(lst->content);
+	free(lst);
+}
+
+/*
+int     main(void)
+{
+	t_list  *lst;
+	t_list  *new_lst;
+
+	lst = ft_lstnew("first");
+	new_lst = ft_lstnew("second");
+	ft_lstadd_front(&lst, new_lst);
+	printf("%s\n", lst->content);
+	printf("%s\n", lst->next->content);
+	return (0);
+}
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 16:39:17 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/16 16:36:30 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/16 16:48:13 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
 
 #endif
