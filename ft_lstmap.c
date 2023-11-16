990dc700822827eda4c128ce6579682fc904e5da
ownleex <ownleex@gmail.com>
2023-11-16 17:32:30 +0100

    Refactor ft_lstmap function

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,36 +6,29 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 17:05:37 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/16 17:29:36 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/16 17:32:13 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*first;
-	t_list	*new;
+	t_list	*changed_list;
+	t_list	*changed_elem;
 
-	if (!f || !del)
-		return (NULL);
-	first = NULL;
+	changed_list = NULL;
 	while (lst)
 	{
-		if (!(new = ft_lstnew((*f)(lst->content))))
+		changed_elem = ft_lstnew(f(lst->content));
+		if (!changed_elem)
 		{
-			while (first)
-			{
-				new = first->next;
-				(*del)(first->content);
-				free(first);
-				first = new;
-			}
-			lst = NULL;
+			ft_lstclear(&changed_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&first, new);
+		ft_lstadd_back(&changed_list, changed_elem);
 		lst = lst->next;
 	}
-	return (first);
+	return (changed_list);
 }
+
