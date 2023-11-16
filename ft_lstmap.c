3e63ca2fd8ba92a7a8da304fbf934be8d6c7fbea
ownleex <ownleex@gmail.com>
2023-11-16 18:31:08 +0100

    Update ft_lstmap function

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 17:05:37 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/16 18:27:17 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/16 18:30:58 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlst;
 	t_list	*tmp;
+	void	*content;
 
 	if (!lst || !f)
 		return (NULL);
 	newlst = NULL;
 	while (lst)
 	{
-		tmp = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		tmp = ft_lstnew(content);
 		if (!tmp)
 		{
 			if (del)
-                		del(lst->content);
+				del(lst->content);
 			ft_lstclear(&newlst, del);
 			return (NULL);
 		}
