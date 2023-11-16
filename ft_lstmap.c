776d575d9cb80351b450390ed2caeb4f6a2eb763
ownleex <ownleex@gmail.com>
2023-11-16 18:21:31 +0100

    Refactor ft_lstmap function

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,34 +6,30 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 17:05:37 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/16 18:17:16 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/16 18:21:24 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+static t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list	*nlst;
-    t_list	*alst;
-    t_list	*tmp;
-    t_list	*tmp1;
+	t_list	*newlst;
+	t_list	*tmp;
 
-    if (lst == NULL || f == NULL)
-        return (NULL);
-    tmp = lst;
-    tmp1 = f(tmp);
-    alst = ft_lstnew(tmp1->content, tmp1->content_size);
-    if (alst == NULL)
-        return (NULL);
-    nlst = alst;
-    tmp = tmp->next;
-    while (tmp)
-    {
-        tmp1 = f(tmp);
-        nlst->next = ft_lstnew(tmp1->content, tmp1->content_size);
-        tmp = tmp->next;
-        nlst = nlst->next;
-    }
-    return (alst);
+	if (!lst || !f)
+		return (NULL);
+	newlst = NULL;
+	while (lst)
+	{
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&newlst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlst, tmp);
+		lst = lst->next;
+	}
+	return (newlst);
 }
