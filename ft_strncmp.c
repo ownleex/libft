f7f7525a2b691018bb7fd7fd2012dfc434363e49
ownleex <ownleex@gmail.com>
2023-11-19 22:58:08 +0100

    Fix strncmp function

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 01:57:05 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/10 23:19:15 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/19 22:57:50 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,11 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 
 	i = 0;
 	if (n == 0)
-	{
 		return (0);
-	}
 	while (i < n && (str1[i] != '\0' && str2[i] != '\0'))
-	{
-		if ((unsigned char)str1[i] != (unsigned char)str2[i])
-		{
-			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
-		}
 		i++;
-	}
 	if (i < n)
-	{
 		return ((unsigned char)str1[i] - (unsigned char)str2[i]);
-	}
 	return (0);
 }
 
