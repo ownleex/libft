51a87327fc8693d0eb78ef94003699316994724a
ownleex <ownleex@gmail.com>
2023-11-19 23:30:51 +0100

    Fix comparison bug in ft_strncmp function

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 01:57:05 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/19 23:02:01 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/19 23:30:43 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (str1[i] && str1[i] == str2[i] && i < n)
+	while (str1[i] != '\0' && str1[i] == str2[i] && i < n)
 		i++;
 	if (i < n)
 		return ((unsigned char)str1[i] - (unsigned char)str2[i]);
