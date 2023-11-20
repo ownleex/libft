402134420a3750195a63da7f5c6f8205b89bfd1b
ownleex <ownleex@gmail.com>
2023-11-20 15:57:40 +0100

    Fix comparison bug in ft_memcmp function

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 01:55:21 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/10 23:21:43 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/20 15:57:29 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int	ft_memcmp(const void *str1, const void *str2, size_t n)
 	s1 = (const unsigned char *)str1;
 	s2 = (const unsigned char *)str2;
 	i = 0;
-	while (i < n && s1[i] == s2[i])
+	while (i < n && (s1 + i) == (s2 + i))
 		i++;
 	if (i < n)
-		return (s1[i] - s2[i]);
+		return ((s1 + i) - (s2 + i));
 	return (0);
 }
 
