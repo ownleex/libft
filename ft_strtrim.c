26acc1342c8e71a17b546d29c404c1fe09bec6a5
ownleex <ownleex@gmail.com>
2023-11-21 01:46:26 +0100

    Refactor ft_strtrim function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:09:52 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/21 01:36:22 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/21 01:46:17 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	i = 0;
 	while (start < end)
-	{
-		str[i] = s1[start];
-		i++;
-		start++;
-	}
+		str[i++] = s1[start++];
 	str[i] = '\0';
 	return (str);
 }
