8aae35870237e2c51a8eaa37f1afd98964b4d4f2
ownleex <ownleex@gmail.com>
2023-11-21 01:34:59 +0100

    Fix strtrim function to correctly copy characters from s1 to str

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:09:52 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/21 01:11:11 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/21 01:34:48 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	i = 0;
 	while (start < end)
-		str[i++] = s1[start++];
+		str[i] = s1[start];
+		i++;
+		start++;
 	str[i] = '\0';
 	return (str);
 }
