316c815b05f24b6290320285140e845c0e96cb6e
ownleex <ownleex@gmail.com>
2023-11-27 23:57:30 +0100

    Fix needle in ft_strnstr function

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:08:38 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/27 23:43:48 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/27 23:57:23 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strnstr(const char *str1, const char *str2, size_t n)
 	size_t	i;
 	size_t	len;
 
-	if (*str2 == 0)
+	if (*str2 == '\0')
 		return ((char *)str1);
 	i = 0;
 	len = ft_strlen(str2);
@@ -34,7 +34,7 @@ char	*ft_strnstr(const char *str1, const char *str2, size_t n)
 int main(void)
 {
     const char *haystack = "Hello, World!";
-    const char *needle = "World";
+    const char *needle = "World!";
     size_t len = 40;
 
     char *result = ft_strnstr(haystack, needle, len);
