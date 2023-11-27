ddff38da6f8cf04d9ef8b0980219b091e8118041
ownleex <ownleex@gmail.com>
2023-11-27 14:37:05 +0100

    Refactor ft_lstmap function

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 17:05:37 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/27 14:34:35 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/27 14:37:00 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newlst;
 	t_list	*tmp;
+	void	*contt;
 
 	if (!lst || !f)
 		return (NULL);
 	newlst = NULL;
 	while (lst)
 	{
-		content = f(lst->content);
-		tmp = ft_lstnew(content);
+		contt = f(lst->content);
+		tmp = ft_lstnew(contt);
 		if (!tmp)
 		{
 			if (del)
-				del(content);
+				del(contt);
 			ft_lstclear(&newlst, del);
 			return (NULL);
 		}
