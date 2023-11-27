963ce2da9f19eb2722f3252ee3ee1f324d27021b
ownleex <ownleex@gmail.com>
2023-11-27 15:33:10 +0100

    Update ft_memcpy function

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 01:55:32 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/27 15:31:32 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/27 15:33:03 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!dest && !src)
+		return (NULL);
 	while (i < n)
 	{
 		*(unsigned char *)(dest + i) = *(unsigned char *)(src + i);
@@ -31,9 +33,10 @@ int main()
 	char src[] = "Bonjour, monde!";
 	char dest[15];
 
-	ft_memcpy(dest, src, 2);
+	ft_memcpy(dest, src, 5);
 	printf("Source: %s\n", src);
 	printf("Destination: %s\n", dest);
+
 	return 0;
 }
 */
