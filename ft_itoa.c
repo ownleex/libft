daa21c6b13d8a90da282e41b8cd20e68bb567f98
ownleex <ownleex@gmail.com>
2023-11-28 14:33:30 +0100

    Fix null termination bug in ft_itoa function

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 17:15:55 by ayarmaya          #+#    #+#             */
-/*   Updated: 2023/11/28 14:31:51 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2023/11/28 14:33:06 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ char	*ft_itoa(int n)
 	result = malloc(sizeof(char) * (len + 1));
 	if (!result)
 		return (NULL);
-	result[len] = '/0';
+	result[len] = '\0';
 	if (nl == 0)
 		result[0] = '0';
 	else
